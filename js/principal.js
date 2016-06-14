3c0b16094e0dee17f89360fbbcf7fbcdde4bd2b2
JaninaCosta <jany_costa90@hotamil.com>
2016-06-14 14:38:23 -0500

    se crea usuario.html

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,8 @@
 
-function load_home(){
+function load_item(){
   document.getElementById("contenido").innerHTML='<object type="text/html" data="item.html" ></object>';
+}
+
+function load_user(){
+  document.getElementById("contenido").innerHTML='<object type="text/html" data="usuario.html" ></object>';
 }
