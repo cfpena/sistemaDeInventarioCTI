95c955a6acd7bee813a8ed66943dabc7b097a17c
JaninaCosta <jany_costa90@hotamil.com>
2016-06-15 04:40:59 -0500

    commit

---
+++ js/crear_item.js
@@ -0,0 +1,3 @@
+function load_items(){
+  document.getElementById("contenido_crear_item").innerHTML='<object type="text/html" data="item.html" ></object>';
+}
--- js/item.js
+++ js/item.js
@@ -1,3 +1,6 @@
+//document.write("<script type='text/javascript' src='principal.js'></script>");
+
 function load_crear_item(){
-  document.getElementById("contenido").innerHTML='<object type="text/html" data="crear_item.html" ></object>';
+  //limpiar();
+  document.getElementById("contenido_item").innerHTML='<object type="text/html" data="crear_item.html" ></object>';
 }
