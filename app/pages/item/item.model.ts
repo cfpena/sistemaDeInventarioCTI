85e0dc1cd8aa96f3cca95f7481fed362154ade9b
vronikp <veronica.pozo@interprofcorp.com>
2016-06-25 18:52:57 -0500

    Lista de Item
    Agregar lista de items con angular

---
+++ app/pages/item/item.model.ts
@@ -0,0 +1,10 @@
+export class ITEM {
+  id: number;
+  codigo: string;
+  nombre: string;
+  marca: string;
+  modelo: string;
+  descripcion: string;
+  cantidad: number;
+  esDispositivo: boolean;
+}
--- app/pages/item/item.ts
+++ app/pages/item/item.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
 import {NavController,MenuController} from 'ionic-angular';
+import {ITEM} from '../item/item.model';
+
 
 @Component({
   templateUrl: 'build/pages/item/item.html'
 })
 export class ItemPage {
+  items = ITEMS;
+  selectedItem : ITEM;
 
+  onSelect (item : ITEM) {this.selectedItem = item;}
   constructor(private _navController:NavController,private menu: MenuController) {}
   openMenu(){
     this.menu.open();
@@ -13,3 +18,9 @@ export class ItemPage {
 
 
 }
+
+const ITEMS: ITEM[]=[
+  {id: 1,  codigo: 'Item001',  nombre: 'Item 1',  marca: 'Marca 1',  modelo: 'Modelo 1',  descripcion: ' ', cantidad:20, esDispositivo: true},
+  {id: 2,  codigo: 'Item002',  nombre: 'Item 2',  marca: 'Marca 1',  modelo: 'Modelo 2',  descripcion: ' ', cantidad:10, esDispositivo: true},
+  {id: 3,  codigo: 'Item003',  nombre: 'Item 3',  marca: 'Marca 2',  modelo: 'Modelo 3',  descripcion: ' ', cantidad:15,esDispositivo: false}
+]
