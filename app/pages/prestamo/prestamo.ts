03295e4bcfe739576a070f9b99f248e39ef073a6
vronikp <veronica.pozo@interprofcorp.com>
2016-06-25 22:07:45 -0500

    Prestamo

--- app/pages/prestamo/prestamo.ts
+++ app/pages/prestamo/prestamo.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ITEM} from '../item/item.model';
+import {Kit} from '../kit/kit.model';
+
+
 
 /*
   Generated class for the PrestamoPage page.
@@ -11,5 +15,29 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/prestamo/prestamo.html',
 })
 export class PrestamoPage {
+  items = ITEMS;
+  kits = KITS;
   constructor(private nav: NavController) {}
 }
+
+const listaItems1: ITEM[]=[
+  {id: 1,  codigo: 'Item001',  nombre: 'Item 1',  marca: 'Marca 1',  modelo: 'Modelo 1',  descripcion: ' ', cantidad:20, esDispositivo: true},
+  {id: 2,  codigo: 'Item002',  nombre: 'Item 2',  marca: 'Marca 1',  modelo: 'Modelo 2',  descripcion: ' ', cantidad:10, esDispositivo: true}
+
+]
+
+const listaItems2: ITEM[]=[
+  {id: 3,  codigo: 'Item003',  nombre: 'Item 3',  marca: 'Marca 2',  modelo: 'Modelo 3',  descripcion: ' ', cantidad:15,esDispositivo: false}
+]
+
+
+const KITS: Kit[]=[
+  {id: 1,  codigo: 'Kit001',  nombre: 'Kit 1',  marca: 'Marca 1',  modelo: 'Modelo 1',  descripcion: ' ', cantidad:20, items: listaItems1, observaciones:''},
+  {id: 2,  codigo: 'Kit002',  nombre: 'Kit 2',  marca: 'Marca 1',  modelo: 'Modelo 2',  descripcion: ' ', cantidad:10, items: listaItems2, observaciones:''}
+  ]
+
+const ITEMS: ITEM[]=[
+  {id: 1,  codigo: 'Item001',  nombre: 'Item 1',  marca: 'Marca 1',  modelo: 'Modelo 1',  descripcion: ' ', cantidad:20, esDispositivo: true},
+  {id: 2,  codigo: 'Item002',  nombre: 'Item 2',  marca: 'Marca 1',  modelo: 'Modelo 2',  descripcion: ' ', cantidad:10, esDispositivo: true},
+  {id: 3,  codigo: 'Item003',  nombre: 'Item 3',  marca: 'Marca 2',  modelo: 'Modelo 3',  descripcion: ' ', cantidad:15,esDispositivo: false}
+]
