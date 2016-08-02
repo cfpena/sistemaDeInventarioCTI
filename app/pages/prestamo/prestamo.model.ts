9100ab7e5995cfda6971898bd8e80bbc5c648b29
JaninaCosta <jany_costa90@hotamil.com>
2016-08-01 19:37:04 -0500

    modificaciones a prestamo

--- app/pages/prestamo/prestamo.model.ts
+++ app/pages/prestamo/prestamo.model.ts
@@ -1,19 +1,31 @@
 import {Validator} from "validator.ts/Validator";
 import {Contains, IsInt, IsLength, IsEmail, IsAlpha} from "validator.ts/decorator/Validation";
+import {ITEM} from '../item/item.model';
+import {Kit} from '../kit/kit.model';
+import {Persona} from '../persona/persona.model';
 
 export class Prestamo {
     @IsInt() id: number;
-    typeIdentificacion: string; //tipo de identificacion de una persona : por nombre o por cedula
-    inputIdentificacion: string;
-    busqueda: string; //busqueda por código o nombre
-    inputbusqueda: string;
+    personas: Persona[];
+    items : ITEM[];
+    kits : Kit[];
+    @IsInt() cantidad: number;
+    disponible: boolean; //si esta disponible para prestar
+    devuelto: boolean;
+    fecha_prestamo: string;
+    fecha_fin: string;
 
-/*
+
 constructor( ) {
+  this.disponible=true;
+  this.devuelto=true;
+  this.personas= [];
+  this.items=[];
+  this.kits=[];
   this.id=0;
-  this.typeIdentificacion='';
-  this.inputIdentificacion='';
-  this.busqueda='';
-  this.inputbusqueda='';
-}*/
+  this.cantidad=0;
+  this.fecha_prestamo='';
+  this.fecha_fin='';
+}
+
 }
