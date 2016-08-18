8bbb4b1e17bde64bef87ae00f6c2de2c64163b01
cristian <cfpena@espol.edu.ec>
2016-08-18 01:20:55 -0500

    items nuevo modelo

--- app/pages/item/item.ts
+++ app/pages/item/item.ts
@@ -1,11 +1,11 @@
-import {Component,  OnInit, Input, ViewChild } from '@angular/core';
-import {NavController, MenuController, Toast, Modal } from 'ionic-angular';
-import {ITEM} from './item.model';
-import {MaterializeDirective} from "../../materialize-directive";
-import {Validator} from "validator.ts/Validator";
-import {Url} from '../../url'
-import {Http, Headers} from '@angular/http';
-import {ItemService} from './item.service';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { NavController, MenuController, Toast, Modal } from 'ionic-angular';
+import { ITEM } from './item.model';
+import { MaterializeDirective } from "../../materialize-directive";
+import { Validator } from "validator.ts/Validator";
+import { Url } from '../../url'
+import { Http, Headers } from '@angular/http';
+import { ItemService } from './item.service';
 
 
 /*
@@ -15,140 +15,142 @@ import {ItemService} from './item.service';
 */
 
 @Component({
-  templateUrl: 'build/pages/item/item.html',
-  directives: [MaterializeDirective],
-  providers: [ItemService],
+    templateUrl: 'build/pages/item/item.html',
+    directives: [MaterializeDirective],
+    providers: [ItemService],
 })
 
 export class ItemPage implements OnInit {
-    title: string ='Ítems';
-    items: ITEM[]=[];
+    title: string = 'Ítems';
+    items:  Array<ITEM> =[]
     template: string = 'null';
-    itemsTemporal: ITEM[]=[];
+    itemsTemporal: ITEM[] = [];
     itemsEliminar: ITEM[] = [];
     @Input()
     itemNuevo = new ITEM();
     @Input()
-    itemModificar= new ITEM;
-    id=0;
-    count=10;
-    selected: number[]=[];
+    itemModificar = new ITEM;
+    id = 0;
+    count = 10;
+    selected: number[] = [];
     tiposBusquedas = ['código', 'Nombre'];
-    busqueda={tipo: 'codigo', valor: ''};
+    busqueda = { tipo: 'codigo', valor: '' };
     tiposFotos = ['archivo', 'cámara'];
-    foto={tipo: 'camara', valor: ''};
-    constructor( private navController:NavController,private menu: MenuController,
-      private itemService: ItemService,
-      private http: Http) {
-        this.itemsTemporal=this.items;
+    foto = { tipo: 'camara', valor: '' };
+    constructor(private navController: NavController, private menu: MenuController,
+        private itemService: ItemService,
+        private http: Http) {
+        this.itemsTemporal = this.items;
     }
     //abre el menu
-  openMenu(){
-    this.menu.open();
-  }
-
-  presentToast(text: string) {
-  let toast = Toast.create({
-    message: text,
-    duration: 3000
-  });
-
-  toast.onDismiss(() => {
-    console.log('Dismissed toast');
-  });
-    this.navController.present(toast);
-  }
-
-  listar() {
-    //las promesas retornan promesas por lo tanto el resultado se debe tratar como una promesa, con el then y catch
-      this.itemService.getItems().then(items => { this.items = items; return items }).then(items => {
-        console.log(items);
-      })
-      return this.items
-  }
-
-  crear(){
-      let validator = new Validator();
-      if(!validator.isValid(this.itemNuevo)) this.presentToast('Corrija el formulario');
-      else if (this.itemNuevo.Codigo=='' || this.itemNuevo.Codigo.length < 10) this.presentToast('Código debe tener 10 dígitos');
-      else if(this.itemNuevo.Nombre=='') this.presentToast('Nombre vacio');
-      else if(this.itemNuevo.Descripcion=='') this.presentToast('Descripción vacio');
-      else if(this.itemNuevo.Stock < 1 || this.itemNuevo.Stock> 50 || this.itemNuevo.Stock==0) this.presentToast('Cantidad mínima 1 máximo 50');
-      else{
-        this.items.push(this.itemNuevo);
-        let item = JSON.parse(JSON.stringify(this.itemNuevo))
-        this.itemService.createItem(item).then(result => this.listar());
-        this.template='null';
-        this.count++;
-        this.itemNuevo = new ITEM();
+    openMenu() {
+        this.menu.open();
     }
-  }
+
+    presentToast(text: string) {
+        let toast = Toast.create({
+            message: text,
+            duration: 3000
+        });
+
+        toast.onDismiss(() => {
+            console.log('Dismissed toast');
+        });
+        this.navController.present(toast);
+    }
+
+    listar() {
+      this.items =[]
+        this.itemService.getElementos().then(items => { this.items = items; return items }).then(result=>{
+          this.itemService.getDispositivos().then(items => {
+            for(var item of items){
+              this.items.push(item)
+            }
+          })
+        })
+    }
+
+    crear() {
+        let validator = new Validator();
+        if (!validator.isValid(this.itemNuevo)) this.presentToast('Corrija el formulario');
+      //  else if (this.itemNuevo.Codigo == '' || this.itemNuevo.Codigo.length < 10) this.presentToast('Código debe tener 10 dígitos');
+        else if (this.itemNuevo.Nombre == '') this.presentToast('Nombre vacio');
+      //  else if (this.itemNuevo.Descripcion == '') this.presentToast('Descripción vacio');
+        else if (this.itemNuevo.Stock < 1 || this.itemNuevo.Stock > 50 || this.itemNuevo.Stock == 0) this.presentToast('Cantidad mínima 1 máximo 50');
+        else {
+            let item = JSON.parse(JSON.stringify(this.itemNuevo))
+            this.itemService.createItem(item).then(result => this.listar());
+            this.template = 'null';
+            this.count++;
+            this.itemNuevo = new ITEM();
+        }
+
+    }
 
     //abre el html de modificar
     goModificar(item: ITEM) {
-      console.log(item)
-            this.itemModificar=JSON.parse(JSON.stringify(item))
-            this.template='modificar'
+        console.log(item)
+        this.itemModificar = JSON.parse(JSON.stringify(item))
+        this.template = 'modificar'
     }
 
-    modificar(){
-      let validator = new Validator();
-      if(!validator.isValid(this.itemModificar)) this.presentToast('Corrija el formulario');
-      else if (this.itemModificar.Codigo=='' || this.itemModificar.Codigo.length < 10)
-      this.presentToast('Código debe tener 10 dígitos');
-      else if(this.itemModificar.Nombre=='') this.presentToast('Nombre vacio');
-      else if(this.itemModificar.Descripcion=='') this.presentToast('Descripción vacio');
-      else if(this.itemModificar.Stock < 1 || this.itemModificar.Stock > 50 || this.itemModificar.Stock==0)
-        this.presentToast('Cantidad mínima 1 máximo 50');
-      else{
-        this.itemService.updateItem(this.itemModificar).then(result => this.listar());
-        this.template='null';
-    }
-  }
+    modificar() {
+        let validator = new Validator();
+        if (!validator.isValid(this.itemModificar)) this.presentToast('Corrija el formulario');
+        //else if (this.itemModificar.Codigo == '' || this.itemModificar.Codigo.length < 10)    this.presentToast('Código debe tener 10 dígitos');
+        else if (this.itemModificar.Nombre == '') this.presentToast('Nombre vacio');
+        //else if (this.itemModificar.Descripcion == '') this.presentToast('Descripción vacio');
+        //else if (this.itemModificar.Stock < 1 || this.itemModificar.Stock > 50 || this.itemModificar.Stock == 0)
+          //  this.presentToast('Cantidad mínima 1 máximo 50');
+        else {
+            this.itemService.updateItem(this.itemModificar).then(result => this.listar());
+            this.template = 'null';
+        }
 
-    eliminar(){
-
-      for(var item of this.itemsEliminar){
-        this.itemService.eliminarItem(item).then(result =>
-          { console.log(result) }).catch(error=> console.log(error))
-      }
-      //se deja en blanco la lista a eliminar
-      this.itemsEliminar= Array<ITEM>();
-      //se refrescan los datos del servidor
-      this.listar();
-      this.listar();
     }
 
+    eliminar() {
 
-  select(item: ITEM) {
-      if (!this.itemsEliminar.some(item => item == item)) {
-          this.itemsEliminar.push(item);
-      }else {
-          let index = this.itemsEliminar.findIndex(x => x == item)
-          this.itemsEliminar.splice(index, 1)
-      };
+        for (var item of this.itemsEliminar) {
+            this.itemService.eliminarItem(item).then(result =>
+            { this.listar()}).catch(error => console.log(error))
+        }
+        //se deja en blanco la lista a eliminar
+        this.itemsEliminar = Array<ITEM>();
 
-  }
-
-      goCrearItem(){
-        this.template='crear';
-      }
-
-    cancelar(){
-      this.template='null';
     }
 
 
-    buscar(){
-      if(this.busqueda.valor.trim() != ""){
-      this.itemService.getBuscar(this.busqueda.valor).then(items => { this.items = items; return items }).then(items => {
-      })}
-      else{this.listar()}
-      return this.items
+    select(item: ITEM) {
+        if (!this.itemsEliminar.some(item => item == item)) {
+            this.itemsEliminar.push(item);
+        } else {
+            let index = this.itemsEliminar.findIndex(x => x == item)
+            this.itemsEliminar.splice(index, 1)
+        };
+
     }
-    
+
+    goCrearItem() {
+        this.template = 'crear';
+    }
+
+    cancelar() {
+        this.template = 'null';
+    }
+
+
+    buscar() {
+        if (this.busqueda.valor.trim() != "") {
+            this.itemService.getBuscar(this.busqueda.valor).then(items => { this.items = items; return items }).then(items => {
+            })
+        }
+        else { this.listar() }
+        return this.items
+    }
+
     //retrasa la carga de la pagina 100 ms
     public ngOnInit() {
-      this.listar();
-  }
-  }
+        this.listar();
+    }
+}
