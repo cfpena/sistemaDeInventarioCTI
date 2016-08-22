f669313f6e7cb7dfdd306fa90beceb7c358ed277
adrianoxav <apinargo@espol.edu.ec>
2016-08-21 23:42:56 -0500

    visualizacion del acta de prestamos incompleta

---
+++ app/pages/prestamo/acta.model.ts
@@ -0,0 +1,11 @@
+import {Validator} from "validator.ts/Validator";
+import {Contains, IsInt, IsLength, IsEmail, IsAlpha} from "validator.ts/decorator/Validation";
+import {ITEM} from '../item/item.model';
+import {Kit} from '../kit/kit.model';
+import {Persona} from '../persona/persona.model';
+
+export class Acta {
+    url: String;
+    Prestador: String;
+    Fecha_vencimiento: String;
+}
---
+++ app/pages/prestamo/devolucion.model.ts
@@ -0,0 +1,21 @@
+import {Validator} from "validator.ts/Validator";
+import {Contains, IsInt, IsLength, IsEmail, IsAlpha} from "validator.ts/decorator/Validation";
+import {ITEM} from '../item/item.model';
+import {Kit} from '../kit/kit.model';
+import {Persona} from '../persona/persona.model';
+
+
+export class Devolucion {
+    url: string;
+    @IsInt() Cantidad: number;
+    Prestamo: string;
+    Detalle: string;
+
+
+constructor( ) {
+  this.Detalle='';
+  this.Cantidad=0;
+
+}
+
+}
