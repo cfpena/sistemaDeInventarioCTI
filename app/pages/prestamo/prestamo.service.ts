6ac6b97ce971a1abf2417db0cb99055891eb0211
adrianoxav <apinargo@espol.edu.ec>
2016-08-22 20:42:23 -0500

    let the pruebas begin!!!

--- app/pages/prestamo/prestamo.service.ts
+++ app/pages/prestamo/prestamo.service.ts
@@ -25,7 +25,7 @@ export class PrestamoService {
               return actas;
             })
         }
-
+/*
         getPrestador(nav: NavController) {
               console.log('get tipos')
                 return this.httprequest.get(this.prestamo.Persona,nav).then(result => {
@@ -33,7 +33,7 @@ export class PrestamoService {
               return personas;
             });
         }
-
+*/
     getPrestamos(nav: NavController) {
       return this.httprequest.get(this.url.base + this.url.prestamo, nav).then(result => {
           let prestamos = result.json() as Prestamo[];
