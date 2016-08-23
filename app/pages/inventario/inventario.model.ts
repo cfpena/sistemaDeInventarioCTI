70f35be88851e6deff70327eefd0aaf6cb4ae68b
cristian <cfpena@espol.edu.ec>
2016-08-23 13:45:33 -0500

    inventario model

--- app/pages/inventario/inventario.model.ts
+++ app/pages/inventario/inventario.model.ts
@@ -1,6 +1,29 @@
 import {Validator} from "validator.ts/Validator";
 import {Contains, IsInt, IsLength, IsEmail, IsAlpha} from "validator.ts/decorator/Validation";
 
+export class Inventario {
+   @IsInt() id: number;
+ fecha: string;
+  codigo: string;
+  tipo: string;
+  nombre: string;
+  marca: string;
+  modelo: string;
+  detalle: string;
+  @IsInt() cantidad: number;
+  estado: string;
+  constructor( ) {
+    this.id=0;
+    this.fecha='';
+    this.codigo='';
+    this.tipo='';
+    this.nombre='';
+    this.marca='';
+    this.modelo='';
+    this.detalle='';
+    this.cantidad=0;
+    this.estado='';}
+}
 
 export class IngresoEgreso {
   url: string;
