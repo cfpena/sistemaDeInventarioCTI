3b18f8d84dcf2d1b5a08cf590b318d2e7b52369d
vronikp <veronica.pozo@interprofcorp.com>
2016-08-25 14:57:34 -0500

    revision inventario

--- app/pages/inventario/inventario.model.ts
+++ app/pages/inventario/inventario.model.ts
@@ -1,29 +1,6 @@
 import {Validator} from "validator.ts/Validator";
 import {Contains,IsDate, IsInt, IsLength, IsEmail, IsAlpha, Matches, NotEmpty} from "validator.ts/decorator/Validation";
 
-export class Inventario {
-  @IsInt() id: number;
-  @IsDate() fecha: string;
-  @Matches(new RegExp("^[a-zA-ZñÑáéíóúÁÉÍÓÚ0-9\s ]*$")) @IsLength(2, 30) Codigo: string;
-  tipo: string;
-  @Matches(new RegExp("^[a-zA-ZñÑáéíóúÁÉÍÓÚ0-9\s ]*$")) @IsLength(2, 30) Nombre: string;
-  @Matches(new RegExp("^[a-zA-ZñÑáéíóúÁÉÍÓÚ0-9\s ]*$")) @IsLength(2, 30) Marca: string;
-  @Matches(new RegExp("^[a-zA-ZñÑáéíóúÁÉÍÓÚ0-9\s ]*$")) @IsLength(2, 30) Modelo: string;
-  @NotEmpty() @Matches(new RegExp("^[a-zA-ZñÑáéíóúÁÉÍÓÚ0-9\s ]*$")) @IsLength(2, 50) detalle: string;
-  @IsInt() cantidad: number;
-  estado: string;
-  constructor( ) {
-    this.id=0;
-    this.fecha='';
-    this.codigo='';
-    this.tipo='';
-    this.nombre='';
-    this.marca='';
-    this.modelo='';
-    this.detalle='';
-    this.cantidad=0;
-    this.estado='';}
-}
 
 export class IngresoEgreso {
   url: string;
--- app/pages/kit/kitelemento.model.ts
+++ app/pages/kit/kitelemento.model.ts
@@ -2,7 +2,6 @@ import {Validator} from "validator.ts/Validator";
 import {Contains, IsInt, IsLength, IsEmail, IsAlpha} from "validator.ts/decorator/Validation";
 import {ITEM} from '../item/item.model';
 import {Kit} from '../kit/kit.model';
-import {Movimiento} from '../inventario/movimiento.model';
 
 
 export class KITELEMENTO {
