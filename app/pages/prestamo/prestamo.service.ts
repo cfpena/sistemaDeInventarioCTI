32145683ff987514b4bcb7a1e07b66f5b365d99c
vronikp <veronica.pozo@interprofcorp.com>
2016-09-05 06:23:01 -0500

    prestamo visualizar y devolver

--- app/pages/prestamo/prestamo.service.ts
+++ app/pages/prestamo/prestamo.service.ts
@@ -12,104 +12,104 @@ import {Persona} from '../persona/persona.model';
 
 @Injectable()
 export class PrestamoService {
-  url = new Url();
-  httprequest:HttpRequest;
+url = new Url();
+httprequest:HttpRequest;
 
-  constructor(private http: Http,
-    private usuarioAuthService: UsuarioAuthService) {
-      this.httprequest = new HttpRequest(http);
+constructor(private http: Http,
+  private usuarioAuthService: UsuarioAuthService) {
+    this.httprequest = new HttpRequest(http);
+  }
+
+  getActas(nav: NavController) {
+    return this.httprequest.get(this.url.base + this.url.acta, nav).then(result => {
+      let actas = result.json() as Acta[];
+      return actas;
+    })
+  }
+
+  createActa1(acta: Acta, nav:NavController){
+    console.log (acta)
+    return this.httprequest.post(this.url.base + this.url.acta, JSON.stringify(acta),nav).then(result => {return result});
+  }
+
+  createActa(acta: Acta, listaPrestamo: Prestamo[], nav:NavController){
+    console.log('voy a crear acta')
+    console.log(acta)
+    return this.httprequest.post(this.url.base + this.url.acta, JSON.stringify(acta),nav).then(result=>{
+        let acta = result.json() as Acta
+        for(let prestamo of listaPrestamo){
+          prestamo.Cantidad = Number(prestamo.Cantidad)
+          prestamo.Acta = acta.url
+          prestamo.Item=prestamo.Item.url
+          console.log(JSON.stringify(prestamo))
+          this.httprequest.post(this.url.base + this.url.prestamo,JSON.stringify(prestamo),nav)
+        }
+    })
+  }
+
+  createDevolucion (listaPrestamos: Prestamo[], nav:NavController){
+    let devolucion;
+    for(let prestamo of listaPrestamos){
+      devolucion = new Devolucion();
+      devolucion.Cantidad=Number(prestamo.Cantidad)
+      devolucion.Prestamo=prestamo.url
+      this.httprequest.post(this.url.base+this.url.devolucion,JSON.stringify(devolucion),nav)
+    }
+  }
+
+  llenarPrestador(acta: Acta,nav: NavController){
+    console.log('llenar prestador')
+    return this.httprequest.get(String(acta.Prestador),nav).then(prestador=>{
+      acta.Prestador =  prestador.json() as Persona;
+    });
+
+  }
+
+  getPrestamos(nav: NavController) {
+    return this.httprequest.get(this.url.base + this.url.prestamo, nav).then(result => {
+      let prestamos = result.json() as Prestamo[];
+      return prestamos;
+    })
+  }
+
+  llenarItem(prestamo: any,nav: NavController){
+    console.log('llenar item')
+    return this.httprequest.get(String(prestamo.Item),nav).then(result=>{
+      prestamo.Item =  result.json() as ITEM;
+      return prestamo;
+    });
+  }
+
+  getBuscar(cadena: String, nav: NavController) {
+    return this.httprequest.get(this.url.base + this.url.prestamo + this.url.buscar + cadena,nav)
+    .then(result => {
+      let prestamos = result.json() as Prestamo[];
+      return prestamos;
+
+    }).catch(error=>{
+      console.log(error)
+    });
+  }
+
+  createPrestamo(prestamo: Prestamo, nav: NavController) {
+    return this.httprequest.post(this.url.base + this.url.prestamo, JSON.stringify(prestamo),nav)
+  }
+
+
+
+  updatePrestamo(prestamo: Prestamo,nav: NavController) {
+    return this.httprequest.patch(String(prestamo.url), JSON.stringify(
+      {
+        Cantidad: prestamo.Cantidad,
+        Fecha: prestamo.Fecha,
+        Detalle: prestamo.Detalle
+
+      }),nav)
+      .then(result => {return result});
     }
 
-    getActas(nav: NavController) {
-      return this.httprequest.get(this.url.base + this.url.acta, nav).then(result => {
-        let actas = result.json() as Acta[];
-        return actas;
-      })
+    eliminarPrestamo(prestamo: Prestamo, nav: NavController) {
+      return this.httprequest.delete(String(prestamo.url),nav)
     }
 
-    createActa1(acta: Acta, nav:NavController){
-      console.log (acta)
-      return this.httprequest.post(this.url.base + this.url.acta, JSON.stringify(acta),nav).then(result => {return result});
-    }
-
-    createActa(acta: Acta, listaPrestamo: Prestamo[], nav:NavController){
-      console.log('voy a crear acta')
-      console.log(acta)
-      return this.httprequest.post(this.url.base + this.url.acta, JSON.stringify(acta),nav).then(result=>{
-          let acta = result.json() as Acta
-          for(let prestamo of listaPrestamo){
-            prestamo.Cantidad = Number(prestamo.Cantidad)
-            prestamo.Acta = acta.url
-            prestamo.Item=prestamo.Item.url
-            console.log(JSON.stringify(prestamo))
-            this.httprequest.post(this.url.base + this.url.prestamo,JSON.stringify(prestamo),nav)
-          }
-      })
-    }
-
-    createDevolucion (listaPrestamos: Prestamo[], nav:NavController){
-      let devolucion;
-      for(let prestamo of listaPrestamos){
-        devolucion = new Devolucion();
-        devolucion.Cantidad=Number(prestamo.Cantidad)
-        devolucion.Prestamo=prestamo.url
-        this.httprequest.post(this.url.base+this.url.devolucion,JSON.stringify(devolucion),nav)
-      }
-    }
-
-    llenarPrestador(acta: Acta,nav: NavController){
-      console.log('llenar prestador')
-      return this.httprequest.get(String(acta.Prestador),nav).then(prestador=>{
-        acta.Prestador =  prestador.json() as Persona;
-      });
-
-    }
-
-    getPrestamos(nav: NavController) {
-      return this.httprequest.get(this.url.base + this.url.prestamo, nav).then(result => {
-        let prestamos = result.json() as Prestamo[];
-        return prestamos;
-      })
-    }
-
-    llenarItem(prestamo: any,nav: NavController){
-      console.log('llenar item')
-      return this.httprequest.get(String(prestamo.Item),nav).then(result=>{
-        prestamo.Item =  result.json() as ITEM;
-        return prestamo;
-      });
-    }
-
-    getBuscar(cadena: String, nav: NavController) {
-      return this.httprequest.get(this.url.base + this.url.prestamo + this.url.buscar + cadena,nav)
-      .then(result => {
-        let prestamos = result.json() as Prestamo[];
-        return prestamos;
-
-      }).catch(error=>{
-        console.log(error)
-      });
-    }
-
-    createPrestamo(prestamo: Prestamo, nav: NavController) {
-      return this.httprequest.post(this.url.base + this.url.prestamo, JSON.stringify(prestamo),nav)
-    }
-
-
-
-    updatePrestamo(prestamo: Prestamo,nav: NavController) {
-      return this.httprequest.patch(String(prestamo.url), JSON.stringify(
-        {
-          Cantidad: prestamo.Cantidad,
-          Fecha: prestamo.Fecha,
-          Detalle: prestamo.Detalle
-
-        }),nav)
-        .then(result => {return result});
-      }
-
-      eliminarPrestamo(prestamo: Prestamo, nav: NavController) {
-        return this.httprequest.delete(String(prestamo.url),nav)
-      }
-
-    }
+  }
--- app/pages/prestamo/prestamo.ts
+++ app/pages/prestamo/prestamo.ts
@@ -18,236 +18,236 @@ import { Acta } from './acta.model';
 import { Devolucion } from './devolucion.model';
 
 @Component({
-  templateUrl: 'build/pages/prestamo/prestamo.html',
-  directives: [MaterializeDirective],
-  providers: [PrestamoService, ,ItemService, PersonaService, UsuarioService],
+templateUrl: 'build/pages/prestamo/prestamo.html',
+directives: [MaterializeDirective],
+providers: [PrestamoService, ,ItemService, PersonaService, UsuarioService],
 
 })
 export class PrestamoPage implements OnInit{
-  title: string ='Prestamos';
+title: string ='Prestamos';
 
-  template: string = 'null';
-  actas:  Array<Acta> =[]
+template: string = 'null';
+actas:  Array<Acta> =[]
 
-  tiposBusquedas = ['Nombre', 'Fecha'];
-  busqueda={tipo: 'Nombre', valor: ''};
+tiposBusquedas = ['Nombre', 'Fecha'];
+busqueda={tipo: 'Nombre', valor: ''};
 
-  estaSeleccionadaPersona=false;
-  descripcionPersona: string ='';
-  personaSeleccionada=new Persona();
-  listaFiltradaPersona:Persona[]=[];
+estaSeleccionadaPersona=false;
+descripcionPersona: string ='';
+personaSeleccionada=new Persona();
+listaFiltradaPersona:Persona[]=[];
 
-  descripcionItem: string ='';
-  itemSeleccionado= new ITEM();
-  estaSeleccionadoItem: boolean=false;
-  listaFiltradaItem: ITEM[]=[];
-  cantidad=0;
+descripcionItem: string ='';
+itemSeleccionado= new ITEM();
+estaSeleccionadoItem: boolean=false;
+listaFiltradaItem: ITEM[]=[];
+cantidad=0;
 
-  listaPrestamos: Prestamo[]=[];
+listaPrestamos: Prestamo[]=[];
 
-  @Input() actaNuevo = new Acta();
+@Input() actaNuevo = new Acta();
 
-  @Input() actaModificar= new Acta();
+@Input() actaModificar= new Acta();
 
-  constructor( private navController:NavController,private menu: MenuController,
-    private prestamoService: PrestamoService, private itemService: ItemService,
-    private personaService: PersonaService, private usuarioService: UsuarioService,
-    private http: Http) {
-    }
+constructor( private navController:NavController,private menu: MenuController,
+  private prestamoService: PrestamoService, private itemService: ItemService,
+  private personaService: PersonaService, private usuarioService: UsuarioService,
+  private http: Http) {
+  }
 
-    openMenu(){
-      this.menu.open();
-    }
+  openMenu(){
+    this.menu.open();
+  }
 
-    goCrearPersona(){
-      //this._navController.push(PersonaPage,{});
-      this.template='crear';
-    }
+  goCrearPersona(){
+    //this._navController.push(PersonaPage,{});
+    this.template='crear';
+  }
 
-    goNuevoPrestamo(){
-      this.template='nuevo_prestamo';
-    }
+  goNuevoPrestamo(){
+    this.template='nuevo_prestamo';
+  }
 
-    goModificarPrestamo(acta: Acta) {
-      console.log(acta)
-      this.actaModificar=JSON.parse(JSON.stringify(acta))
-      this.descripcionPersona = this.actaModificar.Prestador.Nombre + ' ' + this.actaModificar.Prestador.Apellido
-      this.prestamoService.getPrestamos(this.navController).then(prestamos =>{
-        console.log('prestamos')
-        console.log(prestamos)
-        for (var prestamo of prestamos){
-          this.prestamoService.llenarItem(prestamo, this.navController).then(result =>{
-            //prestamo.Item=item;
-            console.log('prestamo acta')
-            console.log(result)
-            console.log('acta')
-            console.log(acta)
-            if (result.Acta==acta.url){
-              this.listaPrestamos.push(result);
-            }
-          })
-        }
-      })
-      this.template='modificar_prestamo'
-    }
-
-    cancelar(){
-      this.listaPrestamos =[];
-      this.descripcionItem='';
-      this.descripcionPersona='';
-      this.template='null';
-    }
-
-    crear(){
-      console.log('crear')
-      console.log (this.actaNuevo)
-      let validator = new Validator();
-      if (!validator.isValid(this.actaNuevo)){ this.presentToast('Corrija el formulario');console.log(validator.validate(this.actaNuevo));}
-      //  else if (this.itemNuevo.Codigo == '' || this.itemNuevo.Codigo.length < 10) this.presentToast('Código debe tener 10 dígitos');
-      //else if (this.kitNuevo.Nombre == '') this.presentToast('Nombre vacio');
-      //  else if (this.itemNuevo.Descripcion == '') this.presentToast('Descripción vacio');
-      //else if (this.kitNuevo.Stock < 1 || this.kitNuevo.Stock > 50 || this.kitNuevo.Stock == 0) this.presentToast('Cantidad mínima 1 máximo 50');
-      else {
-        this.actaNuevo.Prestador=this.personaSeleccionada.url;
-        this.prestamoService.createActa(this.actaNuevo, this.listaPrestamos,this.navController).then(result => {this.listar_actas()});
-        this.template='null';
-        this.actaNuevo= new Acta();
-        this.listaPrestamos =[];
-        this.descripcionItem='';
-        this.descripcionPersona='';
-      }
-    }
-
-    //funcion listar que lista todos los kits creados
-    listar_actas() {
-      this.actas =[]
-      this.prestamoService.getActas(this.navController).then(actas => { this.actas = actas ; return actas}).then(result=>{
-        for(var acta of this.actas){
-          this.prestamoService.llenarPrestador(acta,this.navController)
-        }
-      })
-    }
-
-    devolver(){
-      this.prestamoService.createDevolucion(this.listaPrestamos, this.navController)
-      this.presentToast('Prestamo devuelto')
-      this.listaPrestamos =[];
-      this.descripcionItem='';
-      this.descripcionPersona='';
-      this.template = 'null';
-    }
-
-    buscarItem(){
-      if (this.descripcionItem!==''){
-        this.itemService.getBuscarElemento(this.descripcionItem,this.navController).then(items => { this.listaFiltradaItem = items; return items }).then(items => {
-          this.itemService.getBuscarDispositivo(this.descripcionItem,this.navController).then(items => {
-            for(var item of items){
-              this.listaFiltradaItem.push(item)
-            }
-          })
-        }).then(items => {
-          if (this.listaFiltradaItem.length==0){
-            this.presentToast('El item debe existir en el sistema.');
-            this.descripcionItem='';
+  goModificarPrestamo(acta: Acta) {
+    console.log(acta)
+    this.actaModificar=JSON.parse(JSON.stringify(acta))
+    this.descripcionPersona = this.actaModificar.Prestador.Nombre + ' ' + this.actaModificar.Prestador.Apellido
+    this.prestamoService.getPrestamos(this.navController).then(prestamos =>{
+      console.log('prestamos')
+      console.log(prestamos)
+      for (var prestamo of prestamos){
+        this.prestamoService.llenarItem(prestamo, this.navController).then(result =>{
+          //prestamo.Item=item;
+          console.log('prestamo acta')
+          console.log(result)
+          console.log('acta')
+          console.log(acta)
+          if (result.Acta==acta.url){
+            this.listaPrestamos.push(result);
           }
         })
-
-      }else{
-        this.listaFiltradaItem=[];
       }
-    }
+    })
+    this.template='modificar_prestamo'
+  }
 
-    seleccionarItem(item: ITEM){
-      this.itemSeleccionado=JSON.parse(JSON.stringify(item));
-      this.descripcionItem = this.itemSeleccionado.Codigo +' - '+ this.itemSeleccionado.Nombre;
+  cancelar(){
+    this.listaPrestamos =[];
+    this.descripcionItem='';
+    this.descripcionPersona='';
+    this.template='null';
+  }
+
+  crear(){
+    console.log('crear')
+    console.log (this.actaNuevo)
+    let validator = new Validator();
+    if (!validator.isValid(this.actaNuevo)){ this.presentToast('Corrija el formulario');console.log(validator.validate(this.actaNuevo));}
+    //  else if (this.itemNuevo.Codigo == '' || this.itemNuevo.Codigo.length < 10) this.presentToast('Código debe tener 10 dígitos');
+    //else if (this.kitNuevo.Nombre == '') this.presentToast('Nombre vacio');
+    //  else if (this.itemNuevo.Descripcion == '') this.presentToast('Descripción vacio');
+    //else if (this.kitNuevo.Stock < 1 || this.kitNuevo.Stock > 50 || this.kitNuevo.Stock == 0) this.presentToast('Cantidad mínima 1 máximo 50');
+    else {
+      this.actaNuevo.Prestador=this.personaSeleccionada.url;
+      this.prestamoService.createActa(this.actaNuevo, this.listaPrestamos,this.navController).then(result => {this.listar_actas()});
+      this.template='null';
+      this.actaNuevo= new Acta();
+      this.listaPrestamos =[];
+      this.descripcionItem='';
+      this.descripcionPersona='';
+    }
+  }
+
+  //funcion listar que lista todos los kits creados
+  listar_actas() {
+    this.actas =[]
+    this.prestamoService.getActas(this.navController).then(actas => { this.actas = actas ; return actas}).then(result=>{
+      for(var acta of this.actas){
+        this.prestamoService.llenarPrestador(acta,this.navController)
+      }
+    })
+  }
+
+  devolver(){
+    this.prestamoService.createDevolucion(this.listaPrestamos, this.navController)
+    this.presentToast('Prestamo devuelto')
+    this.listaPrestamos =[];
+    this.descripcionItem='';
+    this.descripcionPersona='';
+    this.template = 'null';
+  }
+
+  buscarItem(){
+    if (this.descripcionItem!==''){
+      this.itemService.getBuscarElemento(this.descripcionItem,this.navController).then(items => { this.listaFiltradaItem = items; return items }).then(items => {
+        this.itemService.getBuscarDispositivo(this.descripcionItem,this.navController).then(items => {
+          for(var item of items){
+            this.listaFiltradaItem.push(item)
+          }
+        })
+      }).then(items => {
+        if (this.listaFiltradaItem.length==0){
+          this.presentToast('El item debe existir en el sistema.');
+          this.descripcionItem='';
+        }
+      })
+
+    }else{
       this.listaFiltradaItem=[];
-      this.estaSeleccionadoItem =true;
     }
+  }
 
-    agregarItem(){
-      if (this.itemSeleccionado){
-        let cant=0;
-        if(Number(this.itemSeleccionado.Stock_Disponible) < Number(this.cantidad)){
+  seleccionarItem(item: ITEM){
+    this.itemSeleccionado=JSON.parse(JSON.stringify(item));
+    this.descripcionItem = this.itemSeleccionado.Codigo +' - '+ this.itemSeleccionado.Nombre;
+    this.listaFiltradaItem=[];
+    this.estaSeleccionadoItem =true;
+  }
+
+  agregarItem(){
+    if (this.itemSeleccionado){
+      let cant=0;
+      if(Number(this.itemSeleccionado.Stock_Disponible) < Number(this.cantidad)){
+        console.log('egreso y mayor que stock disponible');
+        this.presentToast('El item no puede ser agregado. El Stock Disponible es menor que la cantidad.');
+      }else{
+        for (var prestamo of this.listaPrestamos){
+          if (prestamo.Item.url = this.itemSeleccionado.url){
+            cant+=Number(prestamo.Cantidad);
+          }
+        }
+        if(Number(this.itemSeleccionado.Stock_Disponible) < (Number(this.cantidad) +cant)){
           console.log('egreso y mayor que stock disponible');
           this.presentToast('El item no puede ser agregado. El Stock Disponible es menor que la cantidad.');
         }else{
-          for (var prestamo of this.listaPrestamos){
-            if (prestamo.Item.url = this.itemSeleccionado.url){
-              cant+=Number(prestamo.Cantidad);
+          if (this.itemSeleccionado.Es_Dispositivo){
+            for(var _i = 0; _i < this.cantidad; _i++){
+              this.listaPrestamos.push({url:'', Cantidad: 1, Fecha:'',  Detalle: '', Item: this.itemSeleccionado, Acta: this.actaNuevo});
             }
-          }
-          if(Number(this.itemSeleccionado.Stock_Disponible) < (Number(this.cantidad) +cant)){
-            console.log('egreso y mayor que stock disponible');
-            this.presentToast('El item no puede ser agregado. El Stock Disponible es menor que la cantidad.');
           }else{
-            if (this.itemSeleccionado.Es_Dispositivo){
-              for(var _i = 0; _i < this.cantidad; _i++){
-                this.listaPrestamos.push({url:'', Cantidad: 1, Fecha:'',  Detalle: '', Item: this.itemSeleccionado, Acta: this.actaNuevo});
-              }
-            }else{
-              this.listaPrestamos.push({url:'', Cantidad: this.cantidad, Fecha:'',  Detalle: '', Item: this.itemSeleccionado, Acta: this.actaNuevo});
-            }
-            this.descripcionItem = '';
-            this.itemSeleccionado = new ITEM();
-            this.cantidad=0;
-            this.estaSeleccionadoItem = false;
+            this.listaPrestamos.push({url:'', Cantidad: this.cantidad, Fecha:'',  Detalle: '', Item: this.itemSeleccionado, Acta: this.actaNuevo});
           }
+          this.descripcionItem = '';
+          this.itemSeleccionado = new ITEM();
+          this.cantidad=0;
+          this.estaSeleccionadoItem = false;
         }
       }
     }
+  }
 
-    eliminarPrestamo(prestamo: Prestamo){
-      let index=this.listaPrestamos.indexOf(prestamo)
-      this.listaPrestamos.splice(index,1)
-    }
+  eliminarPrestamo(prestamo: Prestamo){
+    let index=this.listaPrestamos.indexOf(prestamo)
+    this.listaPrestamos.splice(index,1)
+  }
 
-    buscarPersona(){
-      if (this.descripcionPersona!==''){
-        this.personaService.getBuscar(this.descripcionPersona, this.navController).then(personas => {this.listaFiltradaPersona=personas; return personas}).then(kits => {
-          if (this.listaFiltradaPersona.length==0){
-            this.presentToast('No existen datos que coincidan con la busqueda. La persona debe estar creada en el sistema.');
-            this.descripcionPersona='';
-          }
-        })
+  buscarPersona(){
+    if (this.descripcionPersona!==''){
+      this.personaService.getBuscar(this.descripcionPersona, this.navController).then(personas => {this.listaFiltradaPersona=personas; return personas}).then(kits => {
+        if (this.listaFiltradaPersona.length==0){
+          this.presentToast('No existen datos que coincidan con la busqueda. La persona debe estar creada en el sistema.');
+          this.descripcionPersona='';
+        }
+      })
 
-      }else{
-        this.listaFiltradaPersona=[];
-      }
-    }
-
-    seleccionarPersona(persona: Persona){
-      this.personaSeleccionada = JSON.parse(JSON.stringify(persona));
-      this.descripcionPersona = this.personaSeleccionada.Nombre + ' ' +this.personaSeleccionada.Apellido;
-      this.estaSeleccionadaPersona=true;
+    }else{
       this.listaFiltradaPersona=[];
     }
+  }
+
+  seleccionarPersona(persona: Persona){
+    this.personaSeleccionada = JSON.parse(JSON.stringify(persona));
+    this.descripcionPersona = this.personaSeleccionada.Nombre + ' ' +this.personaSeleccionada.Apellido;
+    this.estaSeleccionadaPersona=true;
+    this.listaFiltradaPersona=[];
+  }
 
 
-    presentToast(text: string) {
-      let toast = Toast.create({
-        message: text,
-        duration: 3000
-      });
+  presentToast(text: string) {
+    let toast = Toast.create({
+      message: text,
+      duration: 3000
+    });
 
-      toast.onDismiss(() => {
-        console.log('Dismissed toast');
-      });
-      this.navController.present(toast);
+    toast.onDismiss(() => {
+      console.log('Dismissed toast');
+    });
+    this.navController.present(toast);
+  }
+
+  //FUNCION BUSCAR para filtrar en tabla de prestamos principal
+  buscar() {
+    if(this.busqueda.valor.trim() != ""){
+      this.personaService.getBuscar(this.busqueda.valor,this.navController).then(personas => {
+        this.listaFiltradaPersona = personas
+      });}
+      else{this.listar_actas()}
     }
 
-    //FUNCION BUSCAR para filtrar en tabla de prestamos principal
-    buscar() {
-      if(this.busqueda.valor.trim() != ""){
-        this.personaService.getBuscar(this.busqueda.valor,this.navController).then(personas => {
-          this.listaFiltradaPersona = personas
-        });}
-        else{this.listar_actas()}
-      }
-
-      public ngOnInit() {
-        this.listar_actas();
-      }
-
-
-
+    public ngOnInit() {
+      this.listar_actas();
     }
+
+
+
+  }
